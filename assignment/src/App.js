43c4c49c8d982e7211d201ae936edad621298cda
Anup kumar <100834005+anup0404@users.noreply.github.com>
2023-05-09 20:04:16 +0530

    submit-button completed

---
+++ assignment/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SubmitButton from "./Components/submit/SubmitButton";
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <SubmitButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ assignment/src/Components/submit/SubmitButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./SubmitButton.css";
+function SubmitButton() {
+  return (
+    <div className="button-wrap">
+      <button className="button ">Submit</button>
+    </div>
+  );
+}
+
+export default SubmitButton;
